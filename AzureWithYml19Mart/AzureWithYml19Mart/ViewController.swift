fec0ca90598b27b08a66f392fb0516968f3e1e32
erdi ergene <erdieergene@gmail.com>
2024-03-19 23:31:49 +0300

    Hello word

---
+++ AzureWithYml19Mart/AzureWithYml19Mart/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AzureWithYml19Mart
+//
+//  Created by Erdi Ergene on 19.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
